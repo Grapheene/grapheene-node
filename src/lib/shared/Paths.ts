0b406f111a5f9d0a894143416d2904a5158c1573
nabeards <me@nabeards.com>
2022-03-03 16:33:03 -0500

    Paths fix

--- src/lib/shared/Paths.ts
+++ src/lib/shared/Paths.ts
@@ -1,35 +1,7 @@
 import path from 'path'
-import fs from 'fs'
 
-// from https://github.com/lexoyo/node_modules-path
-const pwd = __dirname.split(path.sep);
-const getPath = (moduleName?: string, folder: string[] = pwd) => {
-    if (folder.length < 1) {
-        logError(moduleName, folder);
-        return null;
-    }
-    const nodeModulesPath = folder.concat(["node_modules"]).join(path.sep);
-    const p = moduleName
-        ? path.join(nodeModulesPath, moduleName)
-        : nodeModulesPath;
-    if (fs.existsSync(p)) {
-        return nodeModulesPath;
-    }
-    const res:string = getPath(moduleName, folder.slice(0, -1));
-    if (!res) {
-        logError(moduleName, folder);
-    }
-    return res;
-};
-const logError = (moduleName: string, folder: string[] | null) => {
-    console.error(
-        `Could not find the node_modules folder ${
-            moduleName ? "which contains " + moduleName : ""
-        } in ${folder.join(path.sep)}`
-    );
-}
-
-const node_modules = getPath()
+const isDev = process.env.MODULE_DEV === 'true'
+const node_modules = isDev ? path.join(__dirname, '..', '..', '..', 'node_modules') : path.join(process.cwd(), 'node_modules')
 
 const prismaExec = path.join(node_modules, 'prisma', 'build', 'index.js')
 const prismaStorage = path.dirname(__dirname).replace(/(dist.*)/, 'prisma')
